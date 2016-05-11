55f204b95e3076d402b2a337ae94f4e004f67aa8
Xiangpeng Li <xl1504@nyu.edu>
2016-05-10 23:08:09 -0400

    submit p.a 9

---
+++ Programming Assignment 9/StoryController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\Tag;
+
+use App\Story;
+use App\Location;
+
+class StoryController extends Controller
+{
+    //
+	public function tags() {
+    	$tags = Tag::all();
+    	return view('tags', ['tags' => $tags]);
+    }
+    
+    public function index()
+    {
+        $stories = Story::with('tags')->get();
+        return view('stories', ['stories' => $stories]);
+    }
+
+    public function create()
+    {
+        $locations = Location::all();
+        return view('story.create', ['locations' => $locations]);
+    }
+
+    public function store(Request $request) 
+    {
+        DB::table('stories')->insert([
+        'location_id' => $request->input('location_id'),
+        'title' => $request->input('title'),
+        'story' => $request->input('story'),
+        'published' => $request->input('published'),
+        ]);
+    }
+
+    public function api_stories() {
+        $result = Story::all();
+        return response()->json($result);
+    }   
+}
---
+++ Programming Assignment 9/routes.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/locations', 'LocationController@index');
+Route::get('/tags', 'StoryController@tags');
+Route::get('/stories', 'StoryController@index');
+Route::get('/story/create', 'StoryController@create');
+Route::post('/story/store', 'StoryController@store');
+Route::get('/api/stories', 'StoryController@api_stories');
